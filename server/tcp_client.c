fd3cc7ba3a0987bfc28d7bf443f32b12ef60fa2c
vegas <abdelahe53@gmail.com>
2025-10-30 17:32:27 +0100

    a

--- server/tcp_client.c
+++ server/tcp_client.c
@@ -1,13 +1,23 @@
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
 
 int main()
 {
-	int	socket_door;
-	int	bind_name;
+	int socketFD = socket(AF_UNIX, SOCK_STREAM, 0); //domain, type, protocol.
 
-	//socket
-	socket_door = socket(AF_UNIX, SOCK_STREAM, 0);
+	struct sockaddr_in address;
+	address.sin_familly = AF_UNIX;
+	address.sin_port = 2000;
+	address.sin_addr.s_addr;
+	inet_pton
 
-	//bind
-	bind_name = bind()
+	connect(socketFD, &address, sizeof address);
+
+
+
+
+	return 0;
 }
