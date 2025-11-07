503b4b266cfdb11528bc8d1f7972a656f7cd3f56
vegas <abdelahe53@gmail.com>
2025-11-07 16:08:59 +0100

    a

--- test.c
+++ test.c
@@ -2,24 +2,42 @@
 // 2. bind(2) the socket to a local address and port
 // 3. call listen(2) to put the socket into the listeninig state
 // 4. accept incoming connections using accept(2)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 
-#include <sys/un.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/tcp>
+#define MY_SOCK_PATH "/tmp/somepath"
+#define LISTEN_BACKLOG 50
 
-int sfd_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-if (sfd == -1)
-    handle_error("socket");
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
 
-memset(&my_addr.0, sizeof(my_addr));
-my_addr.sun_family = AF_UNIX;
-strncpy(my_addr.sun_path, MY_SOCK_PATH, sizeof(my_addr.sun_path) - 1);
+int main(void)
+{
+    int sfd;
+    struct sockaddr_un my_addr;
 
-bind(sfd, (struct sockaddr *)&my_addr, sizeof(my_addr) == -1) handle_error("bind");
+    sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sfd == -1)
+        handle_error("socket");
 
-listen((sfd, LISTEN_BACKLOG) == -1) handle_error("listen");
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sun_family = AF_UNIX;
+    strncpy(my_addr.sun_path, MY_SOCK_PATH, sizeof(my_addr.sun_path) - 1);
+
+    if (bind(sfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1)
+        handle_error("bind");
+
+    if (listen(sfd, LISTEN_BACKLOG) == -1)
+    {
+        handle_error("listen");
+        exit(EXIT_FAILURE);
+    }
